0505497af4ecdd080ae21c1c4b49d618f3aedb69
Henrique Diniz <luisdiniz2104@gmail.com>
2026-04-25 09:14:01 -0300

    .
    mais atualizações

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function mostrarProjetos(){
         const lista = document.getElementById("listaProjetos");
         lista.classList.toggle("oculto");
     }
+
+    function carregar(){
+        var msg= window.document.getElementById('msg');
+        var data= new Date();
+        var hora= data.getHours();
+        if(hora>=6 && hora<12){
+            msg.innerHTML='Olá, Bom dia. Eu sou Desenvolvedor Frontend'
+        }else if(hora>=12 && hora<18){
+            msg.innerHTML='Olá, Boa tarde. Eu sou Desenvolvedor Frontend'
+        }else{
+            msg.innerHTML=+'Olá, Boa noite. Eu sou Desenvolvedor Frontend'
+        }
+    }
