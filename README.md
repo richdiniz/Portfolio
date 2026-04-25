bb0734b084d3001ab946ae8d2e6a39a4cce93e6c
Henrique Diniz <luisdiniz2104@gmail.com>
2026-04-24 23:20:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio
+meu portfólio
---
+++ script.js
@@ -0,0 +1,5 @@
+//função de mostrar os projetos feitos...
+function mostrarProjetos(){
+        const lista = document.getElementById("listaProjetos");
+        lista.classList.toggle("oculto");
+    }
